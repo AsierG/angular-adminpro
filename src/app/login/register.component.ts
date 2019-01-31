e2285bcd0f1611568186090c73fbe701f2d99d2c
Asier Garcia <asiergrc@gmail.com>
2019-01-31 16:13:33 +0100

    add reactive forms

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+declare function init_plugins();
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    init_plugins();
+
+    this.registerForm = new FormGroup({
+      nombre: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.email),
+      password2: new FormControl(null, Validators.email),
+      condiciones: new FormControl(false),
+    });
+  }
+
+  userRegister() {
+    console.log(this.registerForm.value);
   }
 
 }
