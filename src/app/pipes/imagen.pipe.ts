6da8d7eb52d4105850ff5d88d606041e0501ff81
Asier Garcia <asiergrc@gmail.com>
2019-02-01 13:50:42 +0100

    finish image pipe

--- src/app/pipes/imagen.pipe.ts
+++ src/app/pipes/imagen.pipe.ts
@@ -1,11 +1,38 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { URL_SERVICES } from '../config/config';
 
 @Pipe({
   name: 'imagen'
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(img: string, type: string = 'user'): any {
+
+    let url = URL_SERVICES + '/img';
+
+    if (!img) {
+      return url + '/usuarios/xxx';
+    }
+
+    if (img.indexOf('https') >= 0) {
+      return img;
+    }
+
+    switch (type) {
+      case 'usuario':
+         url += '/usuarios/' + img;
+         break;
+      case 'medico':
+         url += '/medicos/' + img;
+         break;
+      case 'hospital':
+         url += '/hospitales/' + img;
+         break;
+      default:
+        console.log('Type of image does not exist. usuario, medico, hospital');
+        return url + '/usuarios/xxx';
+    }
+
     return 'WORKS!';
   }
 
